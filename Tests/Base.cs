909568c11ef5aaf60d85e5a19c6e43ef39136f74
Tea <tea.desouza@gmail.com>
2019-10-05 23:15:52 +0800

    Latest changes

--- Tests/Base.cs
+++ Tests/Base.cs
@@ -1,15 +1,18 @@
 using NUnit.Framework;
 
-public class TestBase {
+public class TestBase{
 
-    protected void o (bool arg)           { Assert.That(arg); }
-    protected void o (object x, object y) { Assert.That(x, Is.EqualTo(y)); }
+    protected void o (bool arg)
+    => Assert.That(arg);
+
+    protected void o (object x, object y)
+    => Assert.That(x, Is.EqualTo(y));
+
     #if UNITY_EDITOR
-    //protected static void print(string msg) => UnityEngine.Debug.Log(msg);
-    protected static void print(object msg) => UnityEngine.Debug.Log(msg);
-    //protected static void print(bool msg){
-    //    UnityEngine.Debug.Log(msg);
-    //}
+
+    protected static void print(object msg)
+    => UnityEngine.Debug.Log(msg);
+
     #endif
 
 }
