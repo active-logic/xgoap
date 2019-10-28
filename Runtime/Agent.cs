4ecac7231bfb4c4fcc3ad95d094896fb04a1997c
Tea <tea.desouza@gmail.com>
2019-10-28 16:37:53 +0800

    nit

--- Runtime/Agent.cs
+++ Runtime/Agent.cs
@@ -2,11 +2,13 @@ using System;
 
 namespace Activ.GOAP{
 
-public interface Agent
-{ Func<Cost>[] Actions(); }
+public interface Agent{
+    Func<Cost>[] Actions();
+}
 
-public interface Parametric
-{ Action[] Functions(); }
+public interface Parametric{
+    Action[] Functions();
+}
 
 public interface Clonable<T>{
     T Allocate ();
